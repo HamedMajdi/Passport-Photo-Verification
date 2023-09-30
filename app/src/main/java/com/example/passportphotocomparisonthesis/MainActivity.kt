838fba5ac2643080afa80d9804cafc4be7aa6a6b
Hamed Majdi <hamed90_viva@yahoo.com>
2023-09-30 14:38:27 +0300

    Added NavBar and BottomNavigationView

--- app/src/main/java/com/example/passportphotocomparisonthesis/MainActivity.kt
+++ app/src/main/java/com/example/passportphotocomparisonthesis/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.passportphotocomparisonthesis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.passportphotocomparisonthesis.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.fragmentContainerView)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.selectOrAddPassportFragment,
+                R.id.modelTestFragment,
+                R.id.settingsFragment
+            )
+        )
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
